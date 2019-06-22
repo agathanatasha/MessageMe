2c62feae544bc4f980099702d85e9a1608d08e6d
Natasha Ho <natashaho1@gmail.com>
2019-06-22 18:03:26 -0400

    scroll to the bottom of the messages when reload and append new message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,12 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+scroll_bottom = function () {
+    if ($("#messages").length > 0) {
+        $("#messages").scrollTop($("#messages")[0].scrollHeight);
+    }
+}
+
 $(document).on('turbolinks:load', function () {
     // dropdown in navbar
     $('.ui.dropdown').dropdown();
@@ -27,4 +33,5 @@ $(document).on('turbolinks:load', function () {
             .transition('fade')
             ;
     });
+    scroll_bottom();
 })
