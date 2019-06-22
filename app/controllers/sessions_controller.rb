9b6a950077c557ad223b31800d7ae079c4fc8c13
Natasha Ho <natashaho1@gmail.com>
2019-06-21 22:53:17 -0400

    authentication - able to login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        provided_username = params[:session][:username]
+        provided_password = params[:session][:password]
+        user = User.find_by(username: provided_username)
+        if user and user.authenticate(provided_password)
+            session[:user_id] = user.id
+            flash[:positive] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "There is something wrong with your login information"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:positive] = "You have successfully logged out"
+        redirect_to root_path
+    end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,37 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+    def setup
+        @user = User.create(username: "user", password: "user")
+    end
+
     test "can get login page" do
         get login_path
         assert_template 'sessions/new'
         assert_response :success
+    end
+
+    test "show login page given incorrect login details" do
+        get login_path
+        post login_path, params: { session: { username: "amy", password: "amy"}}
+        assert_template 'sessions/new'
+        assert_select 'div.error'
+    end
+
+    test "show chatroom index page given correct login details" do
+        get login_path
+        post login_path, params: { session: { username: @user.username, password: @user.password}}
+        follow_redirect!
+        assert_template 'chatrooms/index'
+        assert_select 'div.positive'
+        assert session[:user_id] == @user.id
+    end
+
+    test "able to log out" do
+        get login_path
+        post login_path, params: { session: { username: @user.username, password: @user.password}}
+        delete login_path
+        assert session[:user_id] == nil
     end
 end
